12666ef17d9ea2e954f67784183611793c8fc829
PavelKazlouski <65292305+PavelKazlouski@users.noreply.github.com>
2020-10-12 19:36:32 +0300

    Add files via upload

--- 1.9.sql
+++ 1.9.sql
@@ -1,3 +1,4 @@
 USE ex1;
-SELECT group_name FROM student_group
-WHERE group_id IN (SELECT group_id FROM teacher_subject_group WHERE room_number > 100 && room_number < 200);
+SELECT SG.group_name, S.subject_name FROM student_group AS SG, subjects AS S
+WHERE SG.group_id IN (SELECT group_id FROM teacher_subject_group WHERE room_number > 100 && room_number < 200)
+AND S.subject_id IN (SELECT subject_id FROM teacher_subject_group WHERE room_number > 100 && room_number < 200);
